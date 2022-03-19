f07776f202f39200459c6ca6a612755dfea593a3
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2022-03-19 19:42:56 +0530

    Add files via upload

---
+++ class/student.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#include<stdio.h>
+struct stud
+{
+      int rno;
+      
+      char name[20];
+}s;
+int main()
+{
+      FILE *fp;
+      fp=fopen("student.txt","w");
+      printf("Enter record of student:\n\n");
+      printf("\nEnter student number : ");
+      scanf("%d",&s.rno);
+      printf("\nEnter name of student: ");
+      scanf("%s",s.name);
+      
+      fprintf(fp,"%d\n%s\",s.rno,s.name);
+      printf("\nRecord stored in file...");
+      fclose(fp);
+      return 0;
+}
